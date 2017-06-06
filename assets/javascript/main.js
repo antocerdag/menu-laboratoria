4c44715c448c6edad956d7cb60032be46012f78d
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-06 16:48:51 -0400

    javascript listo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,13 @@
-main.js
+function desplegar(){
+	var menuOculto = document.getElementById("menuOculto");
+	var aMenu = Array.from(document.getElementsByClassName("mostrar"));
+
+	menuOculto.addEventListener("click", function(e){
+		e.preventDefault();
+		aMenu.forEach(function(a) {
+			a.classList.toggle("hiden");
+		});
+	});
+}
+
+desplegar();
